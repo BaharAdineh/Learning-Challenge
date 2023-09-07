cd1575d36632d9ac2d8e4ed984c0dfe38c8ec333
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-09-07 22:51:33 +0200

    Create github repository

---
+++ src/test/java/com/challenge/learn/LearningChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
