78c7acd579b7b7eb2c0eb47e2abd55ce1a8ad7a4
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-09-23 16:08:09 +0200

    Adjust user service

--- user-service/src/test/java/com/challenge/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/challenge/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,55 @@
 package com.challenge.userservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import com.challenge.userservice.config.SecurityConfig;
+import com.challenge.userservice.controller.AuthController;
+import com.challenge.userservice.controller.HomeController;
+import com.challenge.userservice.service.TokenService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@WebMvcTest({ HomeController.class, AuthController.class })
+@Import({ SecurityConfig.class, TokenService.class })
 class UserServiceApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void tokenWhenAnonymousThenStatusIsUnauthorized() throws Exception {
+		this.mvc.perform(post("/token")).andExpect(status().isUnauthorized());
 	}
 
+	@Test
+	void tokenWithBasicThenGetToken() throws Exception {
+		MvcResult result = this.mvc.perform(post("/token").with(httpBasic("admin", "admin"))).andExpect(status().isOk()).andReturn();
+
+		assertThat(result.getResponse().getContentAsString()).isNotEmpty();
+	}
+
+	@Test
+	void rootWhenUnauthenticatedThen401() throws Exception {
+		this.mvc.perform(get("/")).andExpect(status().isUnauthorized());
+	}
+
+	@Test
+	public void rootWithBasicStatusIsUnauthorized() throws Exception {
+		this.mvc.perform(get("/").with(httpBasic("dvega", "password"))).andExpect(status().isUnauthorized());
+	}
+
+	@Test
+	@WithMockUser
+	public void rootWithMockUserStatusIsOK() throws Exception {
+		this.mvc.perform(get("/")).andExpect(status().isOk());
+	}
 }
