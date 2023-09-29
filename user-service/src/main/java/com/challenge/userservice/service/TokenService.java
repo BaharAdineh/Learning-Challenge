35a9eeec5a2327095fbad1c19b43ef4d7e675071
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-09-29 14:18:44 +0200

    Fix order-service

--- user-service/src/main/java/com/challenge/userservice/service/TokenService.java
+++ user-service/src/main/java/com/challenge/userservice/service/TokenService.java
@@ -1,33 +1,25 @@
 package com.challenge.userservice.service;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
-import org.springframework.stereotype.Service;
-
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.stream.Collectors;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.stream.Collectors;
 
 @Service
 public class TokenService {
-
+    private final Logger log = LoggerFactory.getLogger(TokenService.class);
     private final JwtEncoder encoder;
-
-    public TokenService(JwtEncoder encoder) {
+    private final JwtDecoder jwtDecoder;
+    public TokenService(JwtEncoder encoder, JwtDecoder jwtDecoder) {
         this.encoder = encoder;
+        this.jwtDecoder = jwtDecoder;
     }
 
     public String generateToken(Authentication authentication) {
@@ -45,4 +37,17 @@ public class TokenService {
         return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 
+
+    public boolean validateToken(String token) {
+        try {
+            jwtDecoder.decode(token);
+            return true; // Token is valid
+        } catch (ExpiredJwtException ex) {
+            log.error("Token has expired: " + ex.getMessage());
+            return false; // Token has expired
+        } catch (JwtException ex) {
+            log.error("Token validation failed: " + ex.getMessage());
+            return false; // Token validation failed for some other reason
+        }
+    }
 }
