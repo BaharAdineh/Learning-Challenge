f081fd2a6c46d7777ea1019435fac2c7f5e99036
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-10-15 22:36:02 +0200

    Fix user-service

---
+++ user-service/src/main/java/com/challenge/userservice/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.challenge.userservice.controller;
+
+import com.challenge.userservice.model.User;
+import com.challenge.userservice.service.TokenService;
+import com.challenge.userservice.exception.UnauthorizedException;
+import com.challenge.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+    private final TokenService tokenService;
+
+    @Autowired
+    public UserController(UserService userService, TokenService tokenService) {
+        this.userService = userService;
+        this.tokenService = tokenService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(@RequestHeader("Authorization") String token) {
+        // Check the validity of the token
+        if (tokenService.validateToken(token)) {
+            return userService.getAllUsers();
+        } else {
+            // Handle unauthorized access, e.g., return an error response
+            throw new UnauthorizedException("Invalid or expired token");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable String id, @RequestHeader("Authorization") String token) {
+        // Check the validity of the token
+        if (tokenService.validateToken(token)) {
+            return userService.getUserById(id);
+        } else {
+            // Handle unauthorized access, e.g., return an error response
+            throw new UnauthorizedException("Invalid or expired token");
+        }
+    }
+
+    // Other controller methods can follow a similar pattern
+
+    // Handle unauthorized exceptions using an exception handler
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+}
--- user-service/src/main/java/com/challenge/userservice/service/UserService.java
+++ user-service/src/main/java/com/challenge/userservice/service/UserService.java
@@ -5,24 +5,38 @@ import com.challenge.userservice.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
+    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(String id) {
+        return userRepository.findById(id);
     }
 
     public User createUser(User user) {
         return userRepository.save(user);
     }
 
-    public User getUserByUsername(String username) {
-        return userRepository.findByUsername(username);
+    public User updateUser(User user) {
+        return userRepository.save(user);
     }
 
-    public Optional<User> getUserById(String userId) {
-        return userRepository.findById(userId);
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
     }
 }
