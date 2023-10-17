6fb1e05f65a77a4cb25c50788a9f99dcd0ec00a5
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-10-17 21:37:37 +0200

    Fix user-service

--- user-service/src/main/java/com/challenge/userservice/service/UserService.java
+++ user-service/src/main/java/com/challenge/userservice/service/UserService.java
@@ -17,40 +17,42 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final TokenService tokenService;
+
+
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,TokenService tokenService ) {
         this.userRepository = userRepository;
+        this.tokenService = tokenService;
     }
+
     public Optional<User> getUserById(String id) {
         return userRepository.findById(id);
     }
-/*
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+    /*
 
-
-
-    public User createUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public User updateUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public void deleteUser(String id) {
-        userRepository.deleteById(id);
-    }
+        public List<User> getAllUsers() {
+            return userRepository.findAll();
+        }
 
 
-    private final TokenService tokenService;
 
-    @Autowired
-    public UserService(TokenService tokenService) {
-        this.tokenService = tokenService;
-    }*/
+        public User createUser(User user) {
+            return userRepository.save(user);
+        }
+
+        public User updateUser(User user) {
+            return userRepository.save(user);
+        }
+
+        public void deleteUser(String id) {
+            userRepository.deleteById(id);
+        }
+
+
+    */
+
 
     public List<User> getAllUsers(@RequestHeader("Authorization") String token) {
         if (tokenService.validateToken(token)) {
