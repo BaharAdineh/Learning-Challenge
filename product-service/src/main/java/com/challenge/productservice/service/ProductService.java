89392a7d9791c1b47d5f50cbe465980cd687845f
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-10-25 21:34:30 +0200

    Fix user-service

--- product-service/src/main/java/com/challenge/productservice/service/ProductService.java
+++ product-service/src/main/java/com/challenge/productservice/service/ProductService.java
@@ -1,7 +1,6 @@
 package com.challenge.productservice.service;
 
 import com.challenge.productservice.model.Product;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
