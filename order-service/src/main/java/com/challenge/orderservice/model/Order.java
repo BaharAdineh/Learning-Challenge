b78c594fe2e2b7ecaf93b3894885ffec52ebf07c
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-12-20 23:16:07 +0100

    Fix user-service

--- order-service/src/main/java/com/challenge/orderservice/model/Order.java
+++ order-service/src/main/java/com/challenge/orderservice/model/Order.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "orders")
 public class Order {
     @Id
-    private String id;
+    private String orderId;
     private String productId;
     private String userId;
     private Product product;
