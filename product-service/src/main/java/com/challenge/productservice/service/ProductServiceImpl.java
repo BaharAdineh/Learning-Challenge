b350b3b71a861a6a779ca4ca5ad8285648387692
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-12-28 23:11:41 +0100

    Fix user-service

--- product-service/src/main/java/com/challenge/productservice/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/challenge/productservice/service/ProductServiceImpl.java
@@ -23,7 +23,7 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public Optional<Product> getProductById(String productId) {
+    public Optional<Product> getProductById(final String productId) {
         return productRepository.findById(productId);
     }
 
