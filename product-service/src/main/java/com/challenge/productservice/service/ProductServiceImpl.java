8f2085f1730be5cb43663e80ba3d70abdf7b61fc
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-12-29 23:16:33 +0100

    Fix user-service

--- product-service/src/main/java/com/challenge/productservice/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/challenge/productservice/service/ProductServiceImpl.java
@@ -33,7 +33,7 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public Product updateProduct(Product product) {
+    public Product updateProduct(final Product product) {
         return productRepository.save(product);
     }
 
